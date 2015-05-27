e5500eebf23657036b288fbfb3109220132f6871
Phoebe Li <phoebeli@Phoebes-MacBook-Pro.local>
2015-05-27 16:21:38 -0400

    setup jest

---
+++ __tests__/app-test.js
@@ -0,0 +1,10 @@
+// jest.dontMock('../app/AppComponent');
+
+// describe('it should display hello world', function() {
+//  it('it should display hello world', function() {
+//    var app = require('../app/AppComponent');
+//    console.log('test goes here');
+//  });
+// });
+// 
+console.log('tests go here');
